8490e80d1c66aac2fa417a019405aa04db335753
MarDrambyan <drambyanmaria@gmail.com>
2024-12-29 00:41:51 +0400

    added finishing touches to API

--- app/main.py
+++ app/main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
 from .routers import driver, circuit, race
 
-app = FastAPI(
-    title="F1 REST API",
-    description="API для управления данными о гонщиках, трассах и гонках Формулы-1",
-    version="1.0.0"
-)
+app = FastAPI(title="F1 REST API")
 
 app.include_router(driver.router)
 app.include_router(circuit.router)
