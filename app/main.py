b00c3ae82cf6409bc3ac1a49f32c0acb9d617efc
MarDrambyan <drambyanmaria@gmail.com>
2025-01-07 00:01:42 +0400

    added ui

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from .routers import driver, circuit, race, analitics
+import os
 
 app = FastAPI(title="F1 REST API")
+
+frontend_path = os.path.join(os.path.dirname(__file__), "..", "frontend")
+app.mount("/frontend", StaticFiles(directory=frontend_path), name="frontend")
 
 app.include_router(analitics.router)
 app.include_router(driver.router)
